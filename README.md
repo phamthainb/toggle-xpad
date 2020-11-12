73e7c146fbb02c281d018be3f75c0637c3a5cfac
phamthainb <phamthai4643@gmail.com>
2020-11-13 01:03:56 +0700

    init first time and config base

---
+++ README.md
@@ -0,0 +1,7 @@
+# Toggle Xpad Note
+A button to easily togle Xpad Note.
+
+## Installation
+* cd `~/.local/share/gnome-shell/extensions/`
+* clone this repo `git clone https://github.com/phamthainb/toogle-xpad.git`
+* Restart the gnome shell environnment (reboot, logout and login again, or `alt+f2` then write `r` and press enter).
---
+++ extension.js
@@ -0,0 +1,41 @@
+const St = imports.gi.St;
+const Main = imports.ui.main;
+const Util = imports.misc.util;
+const Shell = imports.gi.Shell;
+
+let toogleXpad;
+
+function init(extensionMeta) {}
+
+function enable() {
+  toogleXpad = new St.Bin({
+    style_class: "panel-button",
+    reactive: true,
+    can_focus: true,
+    x_fill: true,
+    y_fill: false,
+    track_hover: true,
+  });
+
+  let icon = new St.Icon({
+    icon_name: "document-edit-symbolic",
+    style_class: "system-status-icon",
+  });
+
+  toogleXpad.set_child(icon);
+  toogleXpad.connect("button-press-event", handle);
+
+  Main.panel._rightBox.insert_child_at_index(toogleXpad, 0);
+}
+
+function disable() {
+  Main.panel._rightBox.remove_child(toogleXpad);
+}
+
+function handle() {
+  try {
+    Util.trySpawnCommandLine("xpad --toggle");
+  } catch (err) {
+    Main.notify("Somthing is wrong :(");
+  }
+}
